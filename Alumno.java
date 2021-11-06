69f3e0e25378ef64cc2d8f2ae606d9cb31b99689
Mart1nRH716 <93725330+Mart1nRH716@users.noreply.github.com>
2021-11-06 15:50:16 -0600

    Update Alumno.java

--- Alumno.java
+++ Alumno.java
@@ -4,16 +4,21 @@ public class Alumno {
     private int numeroBoleta;
     private String nombre;
     private ArrayList<Materia> materiaCursa;
+    private Grupo grupo;
+    private int grado;
     
     private static int alumnos=0;
     
-    public Alumno(String m,int boleta){
-        nombre=m;
+    public Alumno(String nombreAl,int boleta,int grado){
+        nombre=nombreAl;
+        this.grado=grado;
         numeroBoleta=boleta;
         materiaCursa= new ArrayList<>();
         alumnos++;
     }
-    
+    public static int numAlumnos(){
+        return alumnos;
+    }
     public void agregarMateria(Materia m){
         materiaCursa.add(m);
     }
@@ -22,9 +27,15 @@ public class Alumno {
         return "Numero de boleta: "+numeroBoleta+"\nNOmbre: "+nombre;
     }
     
+    public int getGrado(){
+        return grado;
+    }
+    
     public void imprimirMaterias (){
+        int i=1;
         for(Materia aux: materiaCursa){
-            System.out.println(aux);
+            System.out.println("Materia ["+i+"] que cursa:"+aux.getMateria());
+            i++;
         }
     }
     
