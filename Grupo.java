17d161243b6a5634e193521977ac1877f71a324a
Mart1nRH716 <93725330+Mart1nRH716@users.noreply.github.com>
2021-11-06 14:50:29 -0600

    Update Grupo.java

--- Grupo.java
+++ Grupo.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
 public class Grupo {
+    private String nombre;
+    private ArrayList <Alumno> alumnos;
+    private Profesor prof;
+    
+    private static int numGrupos=0;
+    
+    public Grupo(String nombre){
+        this.nombre=nombre;
+        numGrupos++;
+        alumnos=new ArrayList<>();
+    }
+    
+    public void asignarProf (Profesor p){
+        prof=p;
+    }
+    
+    public void addAlumno(Alumno a){
+        alumnos.add(a);
+    }
+    
+    public void mostrarAlumnos(){
+        for(Alumno aux : alumnos){
+            System.out.println(aux);
+        }
+    }
     
 }
+
