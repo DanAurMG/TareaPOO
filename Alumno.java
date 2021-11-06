f3131f6935784d0540f7d53b0239b6ca42d60a8b
DanAurMG <raizeremi.15.damg@gmail.com>
2021-11-06 15:12:44 -0600

    Cambios en imports para usar scanner

--- Alumno.java
+++ Alumno.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+import java.util.*;
+
 public class Alumno {
     private int numeroBoleta;
     private String nombre;
--- Profesor.java
+++ Profesor.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+
 public class Profesor {
     private String nombre;
     private ArrayList <Materia> materiaImparte;
