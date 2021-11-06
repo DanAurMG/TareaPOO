0fc190e171340a3d0342e1d1cf0f4900d17c44b9
Mart1nRH716 <93725330+Mart1nRH716@users.noreply.github.com>
2021-11-06 13:55:13 -0600

    Update Materia.java

--- Materia.java
+++ Materia.java
@@ -1,3 +1,21 @@
 public class Materia {
+    private String nombre;
+    private int grado;
+    
+    private static int materias=0;
+    
+    public Materia(String nombre, int grado){
+        this.nombre=nombre;
+        this.grado=grado;
+        materias++;
+    }
+    public static int numMaterias(){
+        return materias;
+    }
+    
+    public String toString (){
+        return "Materia: "+nombre+"\nGrado del Materia: "+grado;
+    }
     
 }
+
